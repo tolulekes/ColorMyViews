8a34ba4e525b84b88f27d54a350e6c5ee381a050
Toluleke <tolulekes@gmail.com>
2021-10-22 17:17:14 -0600

    play around 2

--- app/src/main/java/com/toluleke/colormyviews/MainActivity.kt
+++ app/src/main/java/com/toluleke/colormyviews/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.toluleke.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.toluleke.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.apply {
+            val clickableViews: List<View> =
+                listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText)
+
+            for (item in clickableViews) {
+                item.setOnClickListener { makeColored(it) }
+            }
+            invalidateAll()
+        }
+    }
+
+    private fun makeColored(view: View) {
+
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
