88a6a617220568c6cfafe4f803f352d7c2ca0a64
Toluleke <tolulekes@gmail.com>
2021-10-22 23:25:57 -0600

    done with it

--- app/src/main/java/com/toluleke/colormyviews/MainActivity.kt
+++ app/src/main/java/com/toluleke/colormyviews/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         binding.apply {
             val clickableViews: List<View> =
-                listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText)
+                listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, constraintLayout, redButton, yellowButton, greenButton)
 
             for (item in clickableViews) {
                 item.setOnClickListener { makeColored(it) }
@@ -34,10 +34,17 @@ class MainActivity : AppCompatActivity() {
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
 
+            //Boxes changing colors based on touch
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
+            //Boxes using custom colors for background
+            R.id.redButton -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellowButton -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.greenButton -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
+
+            //Changing the background of the picture
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
